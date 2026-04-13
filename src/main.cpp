b64e085df09835fbd9115b1bddd060c96135dd31
Xuan-Son Nguyen <son@huggingface.co>
2026-04-13 22:23:45 +0200

    feat: show crash reason on boot (#1453)
    ## Summary
    
    If the system reboots from crash, display the reason and tell user to
    include `crash_report.txt` file.
    
    To test this, simply add an `assert(false)` somewhere inside the code.
    
    
    ![screenshot-74453.bmp](https://github.com/user-attachments/files/26160373/screenshot-74453.bmp)
    
    ---
    
    ### AI Usage
    
    While CrossPoint doesn't have restrictions on AI tools in contributing,
    please be transparent about their usage as it
    helps set the right context for reviewers.
    
    Did you use AI tools to help write this code? **NO**

--- src/main.cpp
+++ src/main.cpp
@@ -253,7 +253,6 @@ void setup() {
   }
 
   HalSystem::checkPanic();
-  HalSystem::clearPanic();  // TODO: move this to an activity when we have one to display the panic info
 
   SETTINGS.loadFromFile();
   I18N.loadSettings();
@@ -290,10 +289,13 @@ void setup() {
   APP_STATE.loadFromFile();
   RECENT_BOOKS.loadFromFile();
 
-  // Boot to home screen if no book is open, last sleep was not from reader, back button is held, or reader activity
-  // crashed (indicated by readerActivityLoadCount > 0)
-  if (APP_STATE.openEpubPath.empty() || !APP_STATE.lastSleepFromReader ||
-      mappedInputManager.isPressed(MappedInputManager::Button::Back) || APP_STATE.readerActivityLoadCount > 0) {
+  if (HalSystem::isRebootFromPanic()) {
+    // If we rebooted from a panic, go to crash report screen to show the panic info
+    activityManager.goToCrashReport();
+  } else if (APP_STATE.openEpubPath.empty() || !APP_STATE.lastSleepFromReader ||
+             mappedInputManager.isPressed(MappedInputManager::Button::Back) || APP_STATE.readerActivityLoadCount > 0) {
+    // Boot to home screen if no book is open, last sleep was not from reader, back button is held, or reader activity
+    // crashed (indicated by readerActivityLoadCount > 0)
     activityManager.goHome();
   } else {
     // Clear app state to avoid getting into a boot loop if the epub doesn't load
